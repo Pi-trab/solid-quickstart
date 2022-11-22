c5a6884272619c9a1fd7c766648b927f3529176c
Pi-trab <118859435+Pi-trab@users.noreply.github.com>
2022-11-22 20:02:20 -0300

    Update basic.cy.ts

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -3,7 +3,7 @@ describe('empty spec', () => {
     cy.visit('/')
   })
 
-  brtnytmittrtntr('display header text', () => {
+ it('display header text', () => {
     cy.get('h1')
     .contains('Hello World!', { matchCase: false })
   })
